03956577d1cabf374035896a5e4cdaa67213f5a3
Nicolas Kovacs <info@microlinux.fr>
2019-03-18 07:42:17 +0100

    Script pour générer un certificat LetsEncrypt.

---
+++ el7/config/certbot/mkcert.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# mkcert.sh
+#
+# Nicolas Kovacs, 2019
+#
+# Créer ou renouveler un certificat SSL/TLS Let's Encrypt
+
+# Créer le groupe certs avec le GID 240
+if ! grep -q "^certs:" /etc/group ; then
+  groupadd -g 240 certs
+  echo ":: Ajout du groupe certs."
+  sleep 3
+fi
+
+# Arrêter le serveur Apache
+if ps ax | grep -v grep | grep httpd > /dev/null ; then
+  echo ":: Arrêt du serveur Apache."
+  systemctl stop httpd 1 > /dev/null 2>&1
+  sleep 5
+fi
+
+# Générer ou renouveler un certificat SSL/TLS
+certbot certonly \
+  --non-interactive \
+  --email info@microlinux.fr \
+  --preferred-challenges http \
+  --standalone \
+  --agree-tos \
+  --renew-by-default \
+  --webroot-path /var/www/slackbox-site/html \
+  -d slackbox.fr -d www.slackbox.fr \
+  --webroot-path /var/www/slackbox-mail/html \
+  -d mail.slackbox.fr \
+  --webroot-path /var/www/unixbox-site/html \
+  -d www.unixbox.fr -d unixbox.fr \
+  --webroot-path /var/www/unixbox-mail/html \
+  -d mail.unixbox.fr 
+
+# Définir les permissions
+echo ":: Définition des permissions."
+chgrp -R certs /etc/letsencrypt
+chmod -R g=rx /etc/letsencrypt
+
+# Démarrer Apache
+echo ":: Démarrage du serveur Apache."
+systemctl start httpd
+
