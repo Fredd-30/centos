f9b4ab5a0aa2160246cb6a3a7169380c808ef56a
Nicolas Kovacs <info@microlinux.fr>
2019-07-19 09:22:51 +0200

    Fichiers modèles pour OwnCloud.

---
+++ el7/config/owncloud/config.php
@@ -0,0 +1,8 @@
+  'logtimezone' => 'UTC',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'memcache.locking' => '\\OC\\Memcache\\Redis',
+  'redis' => 
+  array (
+    'host' => 'localhost',
+    'port' => 6379,
+  ),
